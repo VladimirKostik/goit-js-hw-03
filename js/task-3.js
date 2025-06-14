d3ac062fdbe16cf33136dca405db6d6131b6e4a3
Vladimir Kostik <vladimirkostik48@gmail.com>
2025-06-14 20:39:50 +0300

    all

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+function filterArray(numbers, value) {
+    const index = numbers.indexOf(value);
+  if (index !== -1) {
+    return numbers.slice(index + 1);
+  } else {
+    return numbers.filter(number => number > value);
+  }
+}
+
+
+
+
+  console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+  console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+  console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+  console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+  console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
